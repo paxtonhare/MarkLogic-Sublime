3591fcef31770420ee173dbfb4c4b2e247f41ccb
Paxton Hare <paxton@greenllama.com>
2014-09-10 11:06:51 -0400

    Fixing Issue #5. Also expanded out optional parameters into > 1 function call

--- support/build_functions.py
+++ support/build_functions.py
@@ -9,7 +9,6 @@ import json
 import fnmatch
 import xml.etree.ElementTree as ET
 
-
 def make_increment_params(start=1):
 	count = [start - 1]
 	def create_increment_params(match):
@@ -19,10 +18,43 @@ def make_increment_params(start=1):
 
 dir = "/path/to/MarkLogic_pubs/pubs/apidocs"
 
-def get_sigs(filename, files):
+def break_apart_optional_params(sig):
+	m = re.search(r"([^(]+)\((.+)?\)", sig)
+	if m:
+		function_name = m.group(1)
+		if (m.group(2)):
+			params = re.split(r",\s*", m.group(2))
+		else:
+			params = []
+
+	required_params = []
+	optional_params = []
+	for param in params:
+		if re.match(r"\[([^]]+)\],?", param):
+			optional_params.append(re.sub(r"\[|\]", "", param))
+		else:
+			required_params.append(param)
+
+	functions = []
+	previous_optional_params = []
+
+	# add the function with any required params
+	functions.append("%s(%s)" % (function_name, ', '.join(required_params) ))
+
+	for param in required_params:
+		previous_optional_params.append(param)
+
+	# add 1 function for each additional optional parameter
+	for param in optional_params:
+		previous_optional_params.append(param)
+		functions.append("%s(%s)" % (function_name, ', '.join(previous_optional_params)))
+	return functions
+
+def get_sigs(filename, final_functions):
 	root = ET.parse(filename)
 	for e in root.findall('.//xhtml:table', namespaces={'xhtml':"http://www.w3.org/1999/xhtml"}):
 		for se in e.findall('./xhtml:tr/xhtml:td/xhtml:a[@id]', namespaces={'xhtml':"http://www.w3.org/1999/xhtml"}):
+			# sanitize the html into a function string
 			sig = u''.join(e.itertext()).encode('utf-8').strip()
 			sig = ''.join([i if ord(i) < 128 else ' ' for i in sig])
 			sig = re.sub(r"\n", "", sig, re.DOTALL | re.M | re.U)
@@ -30,34 +62,45 @@ def get_sigs(filename, files):
 			sig = re.sub(r"\(\s+", "(", sig)
 			sig = re.sub(r"\s+\)", ")", sig)
 			sig = re.sub(r"\) as.*$", ")", sig)
-			sig = re.sub(r"\[|\]", "", sig)
 
-			desc = re.sub(r" as\s.*?(?=,|\)$)+", "", sig)
-			desc = re.sub(r"^[^(]+", "", desc)
-			if (desc == "()"):
-				desc = None
-			sig = re.sub(r"\$", make_increment_params(), sig)
-			sig = re.sub(r",", "},", sig)
-			sig = re.sub(r"\)$", "})", sig)
+			# if there are optional params, break this thing up into multiple instances
+			signatures = break_apart_optional_params(sig)
+			for signature in signatures:
+				desc = re.sub(r" as\s.*?(?=,|\)$)+", "", signature)
+				desc = re.sub(r"^[^(]+", "", desc)
+				desc = re.sub(r",\s+", ",", desc)
+				if (desc == "()"):
+					desc = None
 
-			function_name = re.match(r"([^(]+)\(", sig).group(1)
+				# wrap the parameters with ${n:} where n is the parameter #
+				if re.search(r"\$", signature):
+					signature = re.sub(r"\$", make_increment_params(), signature)
+					signature = re.sub(r",", "},", signature)
+					signature = re.sub(r"\)$", "})", signature)
 
-			obj = {
-				"content": sig,
-				"trigger": function_name
-			}
-			if (desc):
-				obj['description'] = desc + '                                                    '
+				function_name = re.match(r"([^(]+)\(", signature).group(1)
 
-			files.append(obj)
-	return files
+				obj = {
+					"content": signature,
+					"trigger": function_name
+				}
+				if (desc):
+					obj['description'] = desc
 
-functions = []
+				final_functions[signature] = obj
+	return final_functions
+
+functions = {}
+
 exclude_list = ["All.html", "REST-Resources.html", "management-rest-api.html", "packageREST.html"]
 for root, dirnames, filenames in os.walk(dir):
 	for filename in fnmatch.filter(filenames, '*.html'):
 		if (filename not in exclude_list and re.match(r"RESTclient", filename) == None):
 			get_sigs(dir + "/" + filename, functions)
 
+pruned_functions = []
+for k in sorted(functions):
+	pruned_functions.append(functions[k])
+
 with open('../marklogic_builtins/ml-functions.json', 'w') as f:
-	f.write(json.dumps(functions, sort_keys=True))
+	f.write(json.dumps(pruned_functions, sort_keys=True))
