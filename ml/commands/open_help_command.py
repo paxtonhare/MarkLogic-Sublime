26ad9233413f8c5acae3f1d2b16501841095e60a
Paxton Hare <paxton@greenllama.com>
2014-09-11 11:57:32 -0400

    Fixing Issue #8 Cleaning up the text before sending it to docs.marklogic.com

--- ml/commands/open_help_command.py
+++ ml/commands/open_help_command.py
@@ -1,8 +1,18 @@
 import sublime, sublime_plugin
-
+import re
 import webbrowser
 
 class OpenHelpCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		func = self.view.substr(self.view.word(self.view.sel()[0]))
+		region = self.view.sel()[0]
+		if (region.size() > 0):
+			func = self.view.substr(region)
+		else:
+			func = self.view.substr(self.view.word(region))
+
+		# clean it up a little
+		func = re.sub(r"[\r\n()\[\]]", "", func)
+		func = re.sub(r"\s+$", "", func)
+		if (re.match(r"^[^a-zA-Z]", func)):
+			func = ""
 		webbrowser.open_new_tab('http://docs.marklogic.com/' + func)
--- ml/commands/open_help_search_command.py
+++ ml/commands/open_help_search_command.py
@@ -4,5 +4,9 @@ import webbrowser
 
 class OpenHelpSearchCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		word = self.view.substr(self.view.word(self.view.sel()[0]))
+		region = self.view.sel()[0]
+		if (region.size() > 0):
+			word = self.view.substr(region)
+		else:
+			word = self.view.substr(self.view.word(region))
 		webbrowser.open_new_tab('http://developer.marklogic.com/search?q=' + word + '&p=1')
